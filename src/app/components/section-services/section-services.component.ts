3edf4e13e0fed507028677fd9b0f993cdf2b0254
stephen <sjw948@gmail.com>
2018-07-29 08:55:45 +0100

    separate base CS styles from shared, update imports

--- src/app/components/section-services/section-services.component.ts
+++ src/app/components/section-services/section-services.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-section-services',
   templateUrl: './section-services.component.html',
-  styleUrls: ['./section-services.component.scss']
+  styleUrls: ['./section-services.component.scss', '../../styles/components/_section.scss']
 })
 export class SectionServices {
 
