299173cc2401eb5e8a3e8f97fef7920abdc2a1f6
Stephen Wright <stephen@getawriggleon.com>
2018-07-31 09:04:01 +0100

    transition for about primary images

--- src/app/components/section-about/section-about.component.ts
+++ src/app/components/section-about/section-about.component.ts
@@ -7,5 +7,19 @@ import { Component } from "@angular/core";
 })
 
 export class SectionAbout {
-
+  aboutPrimaryImages: Array<any> = [
+    '/assets/images/about_img_04.jpg',
+    '/assets/images/about_img_05.jpg',
+    '/assets/images/about_img_06.jpg'
+  ]
+  aboutPrimaryImageUrl: string = '/assets/images/about_img_04.jpg';
+  aboutPrimaryImageIndex: number = 0;
+  nextAboutPrimaryImage = (): void => {
+    document.getElementById('about-primary-image-container').classList.add('transitioning');
+    setTimeout(() => {
+      this.aboutPrimaryImageIndex = this.aboutPrimaryImageIndex === this.aboutPrimaryImages.length - 1 ? 0 : this.aboutPrimaryImageIndex + 1;
+      this.aboutPrimaryImageUrl = this.aboutPrimaryImages[this.aboutPrimaryImageIndex]
+      document.getElementById('about-primary-image-container').classList.remove('transitioning');
+    }, 100)
+  }
 }
