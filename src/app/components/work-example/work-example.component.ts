f68e2ca94398a4d0bc4871063b7b0392098eb47e
stephen <sjw948@gmail.com>
2018-07-31 22:25:43 +0100

    work example mobile + desktop

--- src/app/components/work-example/work-example.component.ts
+++ src/app/components/work-example/work-example.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-work-example',
-  templateUrl: './work-example.component.html'
+  templateUrl: './work-example.component.html',
+  styleUrls: ['./work-example.component.scss']
 })
 export class WorkExample {
   
