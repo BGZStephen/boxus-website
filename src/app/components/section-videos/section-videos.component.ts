423df1e6631a8bd5d7ab339bddcd9347cfe6d0bc
stephen <sjw948@gmail.com>
2018-08-01 21:35:17 +0100

    section videos updated

--- src/app/components/section-videos/section-videos.component.ts
+++ src/app/components/section-videos/section-videos.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-section-videos',
   templateUrl: './section-videos.component.html',
-  styleUrls: ['./section-videos.component.scss', '../../styles/components/_section.scss']
+  styleUrls: ['./section-videos.component.scss']
 })
 export class SectionVideos {
 
