9ba33f582aff3c443b18e717a22564530a56e995
stephen <sjw948@gmail.com>
2018-08-01 21:56:43 +0100

    section contact + section skills migrated

--- src/app/components/section-skills/section-skills.component.ts
+++ src/app/components/section-skills/section-skills.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-section-skills',
   templateUrl: './section-skills.component.html',
-  styleUrls: ['./section-skills.component.scss', '../../styles/components/_section.scss'],
+  styleUrls: ['./section-skills.component.scss'],
 })
 export class SectionSkills {
   
