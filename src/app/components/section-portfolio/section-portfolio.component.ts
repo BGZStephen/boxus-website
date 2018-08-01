3378d407119a9cc269dab76bc9e0fd73cbeee7b5
stephen <sjw948@gmail.com>
2018-08-01 07:54:38 +0100

    migrate portfolio + about sections

--- src/app/components/section-portfolio/section-portfolio.component.ts
+++ src/app/components/section-portfolio/section-portfolio.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-section-portfolio',
   templateUrl: './section-portfolio.component.html',
-  styleUrls: ['./section-portfolio.component.scss', '../../styles/components/_section.scss']
+  styleUrls: ['./section-portfolio.component.scss']
 })
 export class SectionPortfolio {
   
--- src/app/components/section-services/section-services.component.ts
+++ src/app/components/section-services/section-services.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import _ from 'lodash';
 
 @Component({
   selector: 'app-section-services',
